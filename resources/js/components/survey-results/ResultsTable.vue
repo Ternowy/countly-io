a332657d3473f134fefd2fecb3f1bf94a9c83712
ternowy <alexander.ternowy@gmail.com>
2021-08-07 20:40:53 +0200

    use table

--- resources/js/components/survey-results/ResultsTable.vue
+++ resources/js/components/survey-results/ResultsTable.vue
@@ -1,21 +1,39 @@
 <template>
   <div class="flex w-full flex-col">
-    <div class="flex w-full flex-col overflow-x-auto">
-      <div class="flex w-full flex-row">
-        <div v-for="(field, labelIndex) in Object.values(structure)" :key="`label-${labelIndex}`"
-             class="border-2 border-black px-10"
-        >
-          <p class="text-base text-grey w-60">{{ field.label }}</p>
-        </div>
-      </div>
+    <div class="flex flex-row w-full overflow-auto" style="height: 75vh;">
+      <table class="w-full">
+        <thead>
+        <tr>
+          <td>
+            <div class="cell-item bg-white">
+              <p class="text-xl text-center text-grey">
+                Created at
+              </p>
+            </div>
+          </td>
+          <td v-for="(field, labelIndex) in Object.values(structure)" :key="`label-${labelIndex}`">
+            <div class="cell-item">
+              <p class="text-xl text-center text-grey">
+                {{ field.label }}
+              </p>
+            </div>
+          </td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(inputs, inputIndex) in data" :key="`inputIndex-${inputIndex}`">
+          <td>
+            <div class="cell-item bg-white">
+              <p class="text-base text-brown">{{ moment(inputs.created_at).format("DD-MM-YYYY HH:m:SS") }}</p>
+            </div>
+          </td>
+          <td v-for="(answer, answerIndex) in inputs.value" :key="`answerIndex-${answerIndex}`" class="content-td">
+            <div class="cell-item"><p class="text-base text-brown">{{ answer.value }}</p></div>
+          </td>
+        </tr>
+        </tbody>
+      </table>
 
-      <div v-for="(inputs, inputIndex) in data" :key="`input-${inputIndex}`" class="flex w-full flex-row">
-        <div v-for="(answer, answerIndex) in inputs" :key="`input-${answerIndex}`"
-             class="border-2 border-black px-10"
-        >
-          <p class="text-base text-grey w-60">{{ answer.value }}</p>
-        </div>
-      </div>
     </div>
     <base-button type="action" class="mt-10" :disabled="!canLoadNextPage" @click.native="loadMore">
       <p class="px-10">Load more</p>
@@ -24,16 +42,19 @@
 </template>
 
 <script>
+import moment from "moment";
+
 export default {
   name: 'ResultsTable',
   props: {
     data: Array,
     structure: Object,
-    canLoadNextPage: Boolean
+    canLoadNextPage: Boolean,
   },
   data () {
     return {
       currentPage: 1,
+      moment
     }
   },
   created () {
@@ -52,7 +73,47 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.table-cell-item {
+.cell-item {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  border-bottom: 1px solid #f2f2f2;
+  padding: 0 20px 0 20px;
+}
 
+tr {
+  background-color: #fff;
+}
+
+tr:nth-child(even) {
+  background-color: #DADADA;
+}
+
+td, th {
+  width: 250px;
+  height: 100px;
+}
+
+table {
+  table-layout: fixed;
+  width: 100%;
+}
+
+td:first-child, th:first-child {
+  position: sticky;
+  left: 0;
+  z-index: 1;
+}
+
+thead tr th {
+  position: sticky;
+  top: 0;
+}
+
+th:first-child {
+  z-index: 2;
 }
 </style>
--- resources/js/service/table-data-formatter.js
+++ resources/js/service/table-data-formatter.js
@@ -2,23 +2,26 @@ class TableDataFormatter {
   formatData (data = [], structure = {}) {
     const fields = Object.keys(structure)
 
-    return data.map(({ inputs, created_at }) =>
-      fields.map(fieldName => {
-        const field = inputs.find(({input_name}) => input_name === fieldName)
+    return data.map(({ inputs, created_at }) => {
+      return {
+        value: fields.map(fieldName => {
+          const field = inputs.find(({input_name}) => input_name === fieldName)
 
-        if (field) {
-          return {
-            value: Array.isArray(field.value)
-              ? field.value.join(', ')
-              : field.value,
+          if (field) {
+            return {
+              value: Array.isArray(field.value)
+                ? field.value.join(', ')
+                : field.value,
+            }
           }
-        }
 
-        return {
-          value: '',
-        }
-      }),
-    )
+          return {
+            value: ' ',
+          }
+        }),
+        created_at
+      }
+    })
   }
 
 }
